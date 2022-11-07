40cf29424913456c363e8868b97981f0943b3ed1
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2022-11-07 19:12:51 +0200

    inverse fourier phase

--- instruments.py
+++ instruments.py
@@ -15,8 +15,12 @@ from scipy.fft import irfft
 import plotly.graph_objects as go
 import streamlit.components.v1 as components
 
+import librosa
+
+import wave
+
 st.set_page_config(
-    page_title="Equalizer",
+    page_title="Music",
     layout="wide")
 
 
@@ -26,8 +30,8 @@ if "slider2" not in st.session_state:
     st.session_state.slider2=0
 if "slider3" not in st.session_state:
     st.session_state.slider3=0
-# if "slider4" not in st.session_state:
-#     st.session_state.slider4=0
+if "slider4" not in st.session_state:
+    st.session_state.slider4=0
 # if "slider5" not in st.session_state:
 #     st.session_state.slider5=0
 # if "slider6" not in st.session_state:
@@ -68,132 +72,142 @@ signal=np.zeros(time.shape)
 
 #Upload wav file
 file=st.file_uploader(label="Upload Signal File", key="uploaded_file",type=["csv","wav"])
-if file:
+if file :
     if file.name.split(".")[-1]=="wav":
-        # signal, time=read_wav(file)
-        sample_rate, signal = wavfile.read(file) 
-        length = signal.shape[0] / sample_rate
-        # time = np.linspace(0., length, signal.shape[0])
-        time =np.linspace(0,signal.shape[0]/sample_rate,signal.shape[0] )
-        st.session_state.signal=signal
-        st.session_state.time= time
-        st.session_state.sample_rate= sample_rate
-        audio_bytes=file.read()
-        st.audio(audio_bytes,format='audio/wav')
+        signal , sample_rate = librosa.load(file, sr=None)
+        # sample_rate, signal = wavfile.read(file) 
+        if signal.ndim==1:
+            length = signal.shape[0] / sample_rate
+            # time = np.linspace(0., length, signal.shape[0])
+            time =np.linspace(0,signal.shape[0]/sample_rate,signal.shape[0] )
+            st.session_state.signal=signal
+            st.session_state.time= time
+            st.session_state.sample_rate= sample_rate
+        else:
+            #2d file conversion
+            signal_1=[]
+            signal_2=[]
+            for i in range (signal.shape[0]):
+                signal_1.append(signal[i][0])
+                signal_2.append(signal[i][1])
+            signal_1=np.array(signal_1)
+            signal_2=np.array(signal_2)    
+            signal=signal_1+signal_2
+
+            length = signal.shape[0] / sample_rate
+            # time = np.linspace(0., length, signal.shape[0])
+            time =np.linspace(0,signal.shape[0]/sample_rate,signal.shape[0] )
+            st.session_state.signal=signal
+            st.session_state.time= time
+            st.session_state.sample_rate= sample_rate
+
+        #display audio file
+        # audio_bytes=file.read()
+        st.audio(file,format='audio/wav')
+
+
 
 #Calculate FOurier Transform
 N = st.session_state.sample_rate * int(st.session_state.time[-1])
-yf = rfft(st.session_state.signal)
-xf = rfftfreq(N, 1 / st.session_state.sample_rate)
-st.session_state.yf_array=yf
-st.session_state.xf_array=xf
+# yf = rfft(st.session_state.signal)
+# xf = rfftfreq(N, 1 / st.session_state.sample_rate)
+# phase=np.angle(st.session_state.yf_array)
+# st.session_state.yf_array=yf
+# st.session_state.xf_array=xf
+
+# Fourier phase and amplitude
+rfft_file = rfft(st.session_state.signal)
+amplitude = np.abs(rfft_file)
+phase = np.angle(rfft_file)
+# frequency = rfftfreq(len(st.session_state.signal), 1/sample_rate)
+frequency = rfftfreq(N, 1/sample_rate)
+
+st.session_state.xf_array=frequency
+
+if "amplitude" not in st.session_state :
+    st.session_state.amplitude=amplitude
 
 
-#Filtering the Signal
+
 # The maximum frequency is half the sample rate
-points_per_freq = len(xf) / (st.session_state.sample_rate / 2)
-# Our target frequency is 400 Hz
-# st.session_state.target_idx_1 = int(points_per_freq * 400)
-# Our target frequency is 600 Hz
-# st.session_state.target_idx_2 = int(points_per_freq * 600)
-# Our target frequency is 2000  Hz
-# st.session_state.target_idx_3 = int(points_per_freq * 2000)
-
+points_per_freq = len(frequency) / (st.session_state.sample_rate / 2)
 
 #upload and view signal
 time_signal_graph, fourier_signal_graph  = st.columns([ 3, 3 ])
 
 # sliders columns
 # slider_1, slider_2, slider_3, slider_4, slider_5, slider_6, slider_7, slider_8, slider_9, slider_10 ,check_boxes = st.columns([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
-
-slider_1, slider_2, slider_3 ,check_boxes = st.columns([4, 4, 4,4])
+slider_1, slider_2, slider_3,slider_4 ,check_boxes = st.columns([4, 4, 4,4,4])
 
 with slider_1 :
     
-    s1= svs.vertical_slider(key="slider1", default_value=0, step=1, min_value=0, 
-                    max_value=220500,   slider_color= 'green',#optional
+    s1= svs.vertical_slider(key="slider1", default_value=0, step=0.1, min_value=0, 
+                    max_value=5,   slider_color= 'green',#optional
                     track_color='lightgray', #optional
                     thumb_color = 'red' #optional,
                     )
 
-    for i in range (50,250):
-        st.session_state.yf_array[int(i*points_per_freq)]=st.session_state.slider1*-1j
-    # new_sig = irfft(st.session_state.yf_array)
-    # norm_new_sig = np.int16(new_sig * (32767 / new_sig.max()))
-    # write("piano.wav", sample_rate, norm_new_sig)
-    # st.audio("piano.wav",format='audio/wav')
+    # st.session_state.amplitude[(frequency>1) & (frequency < 500) ] = amplitude[(frequency>1) & (frequency < 500) ] * (st.session_state.slider2)
+
+    st.session_state.amplitude[int(0*points_per_freq):int(500*points_per_freq)]=  amplitude[int(0*points_per_freq):int(500*points_per_freq)]*(st.session_state.slider1)    
+
+    # st.session_state.yf_array[int(0*points_per_freq):int(500*points_per_freq)]=  st.session_state.yf_array[int(0*points_per_freq):int(500*points_per_freq)]*(st.session_state.slider1)    
+
+    
+    # for i in range (10,499):
+    #     st.session_state.yf_array[int(i*points_per_freq)]=0
     # st.write(st.session_state.yf_array[int(i*points_per_freq)])
-    # st.write("Drums")
+    st.write("Drums")
 
 
 with slider_2 :
-    s2= svs.vertical_slider(key="slider2", default_value=0, step=1, min_value=0, 
-                    max_value=220500,   slider_color= 'red',#optional
+    s2= svs.vertical_slider(key="slider2", default_value=0, step=0.1, min_value=0, 
+                    max_value=5,   slider_color= 'red',#optional
                     track_color='lightgray', #optional
                     thumb_color = 'green' #optional
                     )
-    for i in range (270,450):
-        st.session_state.yf_array[int(i*points_per_freq)]=st.session_state.slider2*-1j
+
+    # st.session_state.amplitude[(frequency>500) & (frequency < 1000) ] = amplitude[(frequency>500) & (frequency < 1000) ] * (st.session_state.slider2)
+
+    st.session_state.amplitude[int(500*points_per_freq):int(1000*points_per_freq)]=  amplitude[int(500*points_per_freq):int(1000*points_per_freq)]*(st.session_state.slider2)    
+
+    # st.session_state.yf_array[int(500*points_per_freq):int(1000*points_per_freq)]=  st.session_state.yf_array[int(500*points_per_freq):int(1000*points_per_freq)]*(st.session_state.slider2)    
+
     # st.write(st.session_state.yf_array[int(i*points_per_freq)]
+    st.write("drums")
+
+with slider_3 :
+    s3= svs.vertical_slider( key="slider3", default_value=0  , step=0.1, min_value=0, 
+    
+                    max_value=5,   slider_color= 'red',#optional
+                    track_color='green', #optional
+                    thumb_color = 'blue' #optional
+                    )
+
+    # st.session_state.amplitude[(frequency>1000) & (frequency < 5000) ] = amplitude[(frequency>1000) & (frequency < 5000) ] * (st.session_state.slider3)
+
+    st.session_state.amplitude[int(1000*points_per_freq):int(5000*points_per_freq)]=  amplitude[int(1000*points_per_freq):int(5000*points_per_freq)]*(st.session_state.slider3)    
+
+    # st.session_state.yf_array[int(1000*points_per_freq):int(5000*points_per_freq)]=  st.session_state.yf_array[int(1000*points_per_freq):int(5000*points_per_freq)]*(st.session_state.slider3)    
+
     st.write("xylophone")
 
-# with slider_3 :
-#     s3= svs.vertical_slider( key="slider3", default_value=0  , step=1, min_value=0, 
-#                     max_value=220500,   slider_color= 'red',#optional
-#                     track_color='green', #optional
-#                     thumb_color = 'blue' #optional
-#                     )
-#     st.session_state.yf_array[st.session_state.target_idx_3]=st.session_state.slider3*-1j
-#     st.write(st.session_state.yf_array[st.session_state.target_idx_3])
+with slider_4 :
+    s3= svs.vertical_slider( key="slider4", default_value=0  , step=1, min_value=0, 
+    
+                    max_value=15,   slider_color= 'red',#optional
+                    track_color='green', #optional
+                    thumb_color = 'blue' #optional
+                    )
 
-# with slider_4 :
-#     s4= svs.vertical_slider( key="slider4", default_value=10, step=1, min_value=0, 
-#                     max_value=100,   slider_color= 'red',#optional
-#                     track_color='green', #optional
-#                     thumb_color = 'blue' #optional
-#                     )
+    # st.session_state.amplitude[(frequency>1000) & (frequency < 5000) ] = amplitude[(frequency>1000) & (frequency < 5000) ] * (st.session_state.slider3)
 
-# with slider_5 :
-#     s5= svs.vertical_slider( key="slider5", default_value=10, step=1, min_value=0, 
-#                     max_value=100,   slider_color= 'red',#optional
-#                     track_color='green', #optional
-#                     thumb_color = 'blue' #optional
-#                     )
+    # st.session_state.amplitude[int(1000*points_per_freq):int(5000*points_per_freq)]=  amplitude[int(1000*points_per_freq):int(5000*points_per_freq)]*(st.session_state.slider3)    
 
-# with slider_6 :
-#     s1= svs.vertical_slider(key="slider6", default_value=50, step=1, min_value=0, 
-#                     max_value=100,   slider_color= 'green',#optional
-#                     track_color='lightgray', #optional
-#                     thumb_color = 'red' #optional
-#                     )
+    # st.session_state.yf_array[int(1000*points_per_freq):int(5000*points_per_freq)]=  st.session_state.yf_array[int(1000*points_per_freq):int(5000*points_per_freq)]*(st.session_state.slider3)    
 
-# with slider_7 :
-#     s2= svs.vertical_slider(key="slider7", default_value=10, step=1, min_value=0, 
-#                     max_value=100,   slider_color= 'red',#optional
-#                     track_color='lightgray', #optional
-#                     thumb_color = 'green' #optional
-#                     )
+    st.write("Volume")
 
-# with slider_8 :
-#     s3= svs.vertical_slider( key="slider8", default_value=10, step=1, min_value=0, 
-#                     max_value=100,   slider_color= 'red',#optional
-#                     track_color='green', #optional
-#                     thumb_color = 'blue' #optional
-#                     )
-
-# with slider_9 :
-#     s4= svs.vertical_slider( key="slider9", default_value=10, step=1, min_value=0, 
-#                     max_value=100,   slider_color= 'red',#optional
-#                     track_color='green', #optional
-#                     thumb_color = 'blue' #optional
-#                     )
-
-# with slider_10 :
-#     s5= svs.vertical_slider( key="slider10", default_value=10, step=1, min_value=0, 
-#                     max_value=100,   slider_color= 'red',#optional
-#                     track_color='green', #optional
-#                     thumb_color = 'blue' #optional
-#                     )
 
 with check_boxes:
     frequency = st.checkbox('frequency', value= True)  
@@ -221,6 +235,14 @@ with check_boxes:
 #     plt.cla()
 #     plt.plot(x,y)
 
+fourier_signal = np.multiply(st.session_state.amplitude, np.exp(1j*phase))
+new_sig = irfft(fourier_signal)
+
+# new_sig = irfft(st.session_state.yf_array)
+# norm_new_sig = np.int16(new_sig * (32767 / new_sig.max()))
+Hear_signal= new_sig *  st.session_state.slider4
+write("new_piano.wav", sample_rate, Hear_signal)
+st.audio("new_piano.wav",format='audio/wav')
 
 #column to draw time graph
 with time_signal_graph:
@@ -233,7 +255,7 @@ with time_signal_graph:
         time= np.linspace(0, 4, 2000)
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=st.session_state.time[0:1000],
-                                y=np.abs(st.session_state.signal[0:1000]),
+                                y=st.session_state.slider4*st.session_state.signal[0:1000],
                                 mode='lines',
                                 name='Signal'))
     fig.update_yaxes(automargin=True)
@@ -244,10 +266,9 @@ with fourier_signal_graph:
     
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=st.session_state.xf_array,
-                                y=np.abs(st.session_state.yf_array),
+                                y=st.session_state.amplitude,
                                 mode='lines',
                                 name='fourier'))
-    fig.update_xaxes(showgrid=True, zerolinecolor='black', gridcolor='lightblue', range = (-0.1,5000))
+    fig.update_xaxes(showgrid=True, zerolinecolor='black', gridcolor='lightblue', range = (-0.1,25000))
     fig.update_yaxes(automargin=True)
     st.plotly_chart(fig,use_container_width=True)
-    
